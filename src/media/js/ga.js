553a848afc10e8e33eeff115d46aa09b01b7af5b
John Karahalis <john.karahalis@gmail.com>
2021-05-02 15:42:28 -0400

    Update to latest openjck/static-site-template

--- src/media/js/ga.js
+++ src/media/js/ga.js
@@ -1,5 +1,9 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', process.env.GA_TRACKING_ID);
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+
+gtag("config", process.env.GA_TRACKING_ID);
